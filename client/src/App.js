90f9d60b05f43f3795860d25cbe6a47e19332bc7
JocaNijeZmaj <100790659+JocaNijeZmaj@users.noreply.github.com>
2023-02-01 10:05:23 +0100

    add-form

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello world</h1>
+      <form className="form-inputs">
+        <div>
+          <label>Name:</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Age:</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Country:</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Wage (year):</label>
+          <input type="text" />
+        </div>
+        <button type="submit">Add Employee</button>
+      </form>
     </div>
   );
 }
